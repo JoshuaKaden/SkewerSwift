e7d19bd49134ec3b637d2792282d99de7f2092f1
Joshua Kaden <jkaden@doitt.nyc.gov>
2017-01-06 17:13:19 -0500

    Initial Commit

---
+++ SkewerSwiftTests/SkewerSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  SkewerSwiftTests.swift
+//  SkewerSwiftTests
+//
+//  Created by Kaden, Joshua on 1/6/17.
+//  Copyright © 2017 Kaden, Joshua. All rights reserved.
+//
+
+import XCTest
+@testable import SkewerSwift
+
+class SkewerSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
