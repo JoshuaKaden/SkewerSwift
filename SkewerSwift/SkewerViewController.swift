4c2103d6b40d6d5d323788c7ff2e1275be11965c
JoshuaKaden <jkaden@kadenspace.com>
2017-03-17 16:33:57 -0400

    updated title

--- SkewerSwift/SkewerViewController.swift
+++ SkewerSwift/SkewerViewController.swift
@@ -21,7 +21,7 @@ final class SkewerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        title = NSLocalizedString("Skewer", comment: "")
+        title = NSLocalizedString("New Title", comment: "")
         
         let gesture = UITapGestureRecognizer(target: self, action: #selector(didDoubleTap(_:)))
         gesture.numberOfTapsRequired = 2
